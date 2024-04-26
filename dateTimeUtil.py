9c72f3da13bb790707466e6fb12f3be67ec3aa82
unknown <orugantialekhya15@gmail.com>
2024-04-26 13:01:55 -0700

    Revert " Scheduling agent initial commit"
    This reverts commit c871cd00184b9699e78785bd7cb363e4f5fea9d3.

--- dateTimeUtil.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import re
-
-def getDateFromStringISOFormat(datetime_str):
-    return datetime.fromisoformat(datetime_str)
-
-def getISOString(dateObject):
-    return dateObject.isoformat()
